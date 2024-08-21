035a69946beae8a943b48232b9d188c465ad7473
Eddrich Janzzen Ang <eddrichang@gmail.com>
2024-08-21 22:36:09 +0800

    Update problems

---
+++ algorithms/utopian-tree/Main.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'utopianTree' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def utopianTree(n):
+    # Write your code here
+    
+    height = 0
+    for x in range(0, n+1):
+        if x % 2 == 0:
+            height = height + 1
+        else:
+            height = height * 2
+        
+    return height
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = utopianTree(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
